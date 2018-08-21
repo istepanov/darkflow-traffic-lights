db008ebe95ea43628ce426eee1e66831b9cb9ec9
Ilya Stepanov <dev@ilyastepanov.com>
2018-08-21 01:05:41 -0700

    Tuning up

--- train.sh
+++ train.sh
@@ -2,9 +2,13 @@
 
 ./flow \
 	--model cfg/yolov2-tiny-traffic-lights.cfg \
-	--load bin/tiny-yolo.weights \
+	--load -1 \
 	--train \
 	--trainer adam \
 	--dataset data/bosch_dataset \
 	--annotation data/bosch_dataset/annotations \
 	--gpu 1.0
+
+
+
+# ./flow --imgdir sample_img/ --model cfg/yolov2-tiny-traffic-lights.cfg --load -1
